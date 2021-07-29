ae7a24e2cf6bb50daff18c278b2d17b626052112
rohanda <s-rohanda.abdallah@zewailcity.edu.eg>
2021-07-29 23:03:23 +0200

    Link prepare data page with controllers

--- WuzzufApp/src/main/java/com/wuzzufjobs/WuzzufController.java
+++ WuzzufApp/src/main/java/com/wuzzufjobs/WuzzufController.java
@@ -7,7 +7,9 @@ package com.wuzzufjobs;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
@@ -24,22 +26,51 @@ import smile.data.DataFrame;
  * @author rohanda
  * 
  */
-@RestController
-
+@Controller
 
 public class WuzzufController {
-
-	@GetMapping(value = "/")
-	public String getData() throws IOException, URISyntaxException {
+        @RequestMapping(value = "/")
+	public String Home(Map<String, Object> model) throws IOException, URISyntaxException {
             PrepareData cdata = new PrepareData();
-            DataFrame wuzzuf = cdata.read_csv("src/main/resources/Wuzzuf_Jobs.csv");
-            HashMap<String, Object> data = new HashMap<String, Object>();
-		data.put("labels", wuzzuf.names());
-		data.put("values", "HI");
-
-		JSONObject response = new JSONObject(data);
-                
             return "index";
+        }
+
+	@RequestMapping(value = "/displayData")
+	public String getData(Map<String, Object> model) throws IOException, URISyntaxException {
+            PrepareData cdata = new PrepareData();
+            DataFrame wuzzuf = cdata.read_csv("src/main/resources/static/Wuzzuf_Jobs.csv");
+            ManipulateData mdata = new ManipulateData();
+            DataFrame cleaned_data = mdata.clean_data(wuzzuf);
+            
+          // HashMap<String, Object> data = new HashMap<String, Object>();
+		//data.put("labels", wuzzuf.names());
+		//data.put("values", "HI");
+            //model.put("DataStructure", dm.convertTable2StringList(structure));
+            //model.put("StructureHeads", dm.getTableHeads(structure));
+                  //  model.put("DataSummary", dm.convertTable2StringList(summary));
+		//model.put("SummaryHeads", dm.getTableHeads(summary));
+               String[] titles =  cleaned_data.apply("Title").toStringArray();
+               String[] companies =  cleaned_data.apply("Company").toStringArray();
+               String[] locations =  cleaned_data.apply("Location").toStringArray();
+               String[] types =  cleaned_data.apply("Type").toStringArray();
+               String[] level =  cleaned_data.apply("Level").toStringArray();
+               String[] years_of_Exp =  cleaned_data.apply("YearsExp").toStringArray();
+              String[] countries =  cleaned_data.apply("Country").toStringArray();
+              String[] skills =  cleaned_data.apply("Skills").toStringArray();
+
+
+
+         
+		model.put("titles", titles);
+                model.put("companies", companies);
+                model.put("locations", locations);
+                model.put("level", level);
+                model.put("types", types);
+                model.put("years_of_Exp", years_of_Exp);
+                model.put("countries", countries);
+                model.put("skills", skills);
+                
+            return "read";
 	}
     
     
