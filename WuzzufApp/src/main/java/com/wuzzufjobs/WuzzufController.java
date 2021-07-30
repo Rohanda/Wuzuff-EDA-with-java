015341b94ff6bbe8079f3771069e26937b1acc8b
rohanda <s-rohanda.abdallah@zewailcity.edu.eg>
2021-07-30 02:05:58 +0200

    Create html bar chart for titles

--- WuzzufApp/src/main/java/com/wuzzufjobs/WuzzufController.java
+++ WuzzufApp/src/main/java/com/wuzzufjobs/WuzzufController.java
@@ -8,13 +8,17 @@ package com.wuzzufjobs;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -72,6 +76,39 @@ public class WuzzufController {
                 
             return "read";
 	}
+        @RequestMapping(value = "/titleChart")
+	public String title_chart(Model model) throws IOException, URISyntaxException {
+            PrepareData cdata = new PrepareData();
+            DataFrame wuzzuf = cdata.read_csv("src/main/resources/static/Wuzzuf_Jobs.csv");
+            ManipulateData mdata = new ManipulateData();
+            DataFrame cleaned_data = mdata.clean_data(wuzzuf);
+            Map<String, Integer> count_title = mdata.count_title(wuzzuf);
+            Set<String> a = count_title.keySet();
+            List<String> x_values = new ArrayList<>(a);
+            Collection<Integer> b = count_title.values();
+            List<Integer> y_values = new ArrayList<>(b);
+            List<List>  chart_data = new ArrayList<List>();
+            /*
+            for(int i = 0;i<5;i++){
+                chart_data.add(new ArrayList<>(Arrays.asList(x_values.get(i),y_values.get(i))));
+                
+            }
+             
+*/
+            HashMap sub_titles = new HashMap();
+            int i =0;
+            for (String name : count_title.keySet()){
+                if (i > 4)
+                    break;
+                sub_titles.put(name,count_title.get(name));
+                i++;
+            }
+            model.addAttribute("chartData", sub_titles); 
+         
+                
+            //model.addAttribute("chartData", getChartData()); 
+            return "popularjobtitle";
+        }
     
     
 }
